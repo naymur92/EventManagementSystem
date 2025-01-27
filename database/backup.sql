23642f3748f76f2393ff5cb9893fce9712778ee5
Naymur Rahman <naymurrahman@yahoo.com>
2025-01-27 09:15:26 +0600

    uploaded latest db backup

--- database/backup.sql
+++ database/backup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 26, 2025 at 04:20 AM
+-- Generation Time: Jan 27, 2025 at 03:14 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -34,8 +34,8 @@ CREATE TABLE `files` (
   `filepath` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `filename` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `fileinfo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_by` smallint UNSIGNED NOT NULL,
-  `deleted_by` smallint UNSIGNED DEFAULT NULL,
+  `created_by` bigint UNSIGNED NOT NULL,
+  `deleted_by` bigint UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -45,11 +45,24 @@ CREATE TABLE `files` (
 --
 
 INSERT INTO `files` (`file_id`, `operation_name`, `table_id`, `filepath`, `filename`, `fileinfo`, `created_by`, `deleted_by`, `created_at`, `deleted_at`) VALUES
-(2, 'users', 1, 'users', '1_1737864125.jpg', 'profile_picture', 1, NULL, '2025-01-25 22:02:05', NULL),
-(3, 'users', 1, 'users', '1_1737864521.jpg', 'profile_picture', 1, NULL, '2025-01-25 22:08:41', NULL),
-(4, 'users', 1, 'users', '1_1737864658.jpg', 'profile_picture', 1, NULL, '2025-01-25 22:10:58', NULL),
-(5, 'users', 1, 'users', '1_1737864879.jpg', 'profile_picture', 1, NULL, '2025-01-25 22:14:39', NULL),
-(6, 'users', 1, 'users', '1_1737865116.jpg', 'profile_picture', 1, NULL, '2025-01-25 22:18:36', NULL);
+(2, 'users', 1, 'users', '1_1737864125.jpg', 'profile_picture', 1, 1, '2025-01-25 22:02:05', '2025-01-26 10:05:30'),
+(3, 'users', 1, 'users', '1_1737864521.jpg', 'profile_picture', 1, 1, '2025-01-25 22:08:41', '2025-01-26 10:05:30'),
+(4, 'users', 1, 'users', '1_1737864658.jpg', 'profile_picture', 1, 1, '2025-01-25 22:10:58', '2025-01-26 10:05:30'),
+(5, 'users', 1, 'users', '1_1737864879.jpg', 'profile_picture', 1, 1, '2025-01-25 22:14:39', '2025-01-26 10:05:30'),
+(6, 'users', 1, 'users', '1_1737865116.jpg', 'profile_picture', 1, 1, '2025-01-25 22:18:36', '2025-01-26 10:05:30'),
+(7, 'users', 1, 'users', '1_1737907625.jpg', 'profile_picture', 1, NULL, '2025-01-26 10:07:05', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `host_details`
+--
+
+CREATE TABLE `host_details` (
+  `user_id` bigint UNSIGNED NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `location` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -58,15 +71,16 @@ INSERT INTO `files` (`file_id`, `operation_name`, `table_id`, `filepath`, `filen
 --
 
 CREATE TABLE `users` (
-  `user_id` smallint UNSIGNED NOT NULL,
+  `user_id` bigint UNSIGNED NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `mobile` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `status` tinyint NOT NULL DEFAULT '0' COMMENT '0=>inactive, 1=>active',
+  `type` tinyint NOT NULL DEFAULT '3' COMMENT '1=>superuser, 2=>host, 3=>general_user',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_by` smallint NOT NULL DEFAULT '0',
-  `updated_by` smallint NOT NULL DEFAULT '0',
+  `created_by` bigint NOT NULL DEFAULT '0',
+  `updated_by` bigint NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -75,12 +89,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `name`, `email`, `mobile`, `email_verified_at`, `status`, `password`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
-(1, 'Naymur Rahman', 'naymur92@gmail.com', '01737036324', NULL, 1, '$2y$10$qQji/ujnkB6upiQXyENnR.UHy.XkqIx1YGiSpDbCv/F0616FvnLiG', 0, 0, '2025-01-23 01:51:37', '2025-01-23 01:51:37'),
-(3, 'Kamrul Hasan', 'kamrul@gmail.com', '01558981652', NULL, 0, '$2y$10$qQji/ujnkB6upiQXyENnR.UHy.XkqIx1YGiSpDbCv/F0616FvnLiG', 0, 1, '2025-01-24 09:27:53', '2025-01-25 07:41:37'),
-(4, 'Abdur Rahman', 'abdrahman@gmail.com', '01737036324', NULL, 0, '$2y$10$6ZHQFuUJDRq5mdYCmsJ5lu/cli9URQ7fG4aH5mto3HEjmmeeIOPuC', 1, 1, '2025-01-25 05:35:22', '2025-01-25 08:35:39'),
-(6, 'Abdur Rahman', 'naymur@gmail.com', '', NULL, 0, '$2y$10$snZMAYf93NtOW3MyKpCr6eafO.895r.IMrj2fGZo4R2p322AXLQCK', 1, 0, '2025-01-25 08:28:34', '2025-01-25 08:28:34'),
-(7, 'Sabbir Saad', 'saad@gmail.com', '', NULL, 0, '$2y$10$2j3g8ESbh3ktyotkh9akrOPhKyOlLbsGFWvccqqPkCQBcDrKrLKVu', 0, 0, '2025-01-25 19:36:10', '2025-01-25 19:36:10');
+INSERT INTO `users` (`user_id`, `name`, `email`, `mobile`, `email_verified_at`, `status`, `type`, `password`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, 'Naymur Rahman', 'naymur92@gmail.com', '01737036324', NULL, 1, 1, '$2y$10$.HRiuajTuUBzB6QcgjoXcu0jMasD5XZZSymxn0jZkNZ12rdI0qVva', 0, 1, '2025-01-23 01:51:37', '2025-01-26 12:46:40'),
+(3, 'Md. Kamrul Hasan', 'kamrul@gmail.com', '01558981652', NULL, 0, 2, '$2y$10$qQji/ujnkB6upiQXyENnR.UHy.XkqIx1YGiSpDbCv/F0616FvnLiG', 0, 1, '2025-01-24 09:27:53', '2025-01-26 12:03:58'),
+(4, 'Abdur Rahman', 'abdrahman@gmail.com', '01737036324', NULL, 0, 3, '$2y$10$6ZHQFuUJDRq5mdYCmsJ5lu/cli9URQ7fG4aH5mto3HEjmmeeIOPuC', 1, 1, '2025-01-25 05:35:22', '2025-01-25 08:35:39'),
+(6, 'Abdur Rahman', 'naymur@gmail.com', '', NULL, 1, 3, '$2y$10$snZMAYf93NtOW3MyKpCr6eafO.895r.IMrj2fGZo4R2p322AXLQCK', 1, 0, '2025-01-25 08:28:34', '2025-01-25 08:28:34'),
+(7, 'Sabbir Saad', 'saad@gmail.com', '', NULL, 0, 2, '$2y$10$2j3g8ESbh3ktyotkh9akrOPhKyOlLbsGFWvccqqPkCQBcDrKrLKVu', 0, 1, '2025-01-25 19:36:10', '2025-01-26 20:28:54'),
+(10, 'Test User', 'test@gmail.com', '', NULL, 1, 2, '$2y$10$exPSoX9MyOnQL8ttMd7MYu9iwbghEO6jwOIn.W8mzLMdfWho4lxzi', 1, 0, '2025-01-26 20:26:43', '2025-01-26 20:26:43');
 
 --
 -- Indexes for dumped tables
@@ -93,6 +108,12 @@ ALTER TABLE `files`
   ADD PRIMARY KEY (`file_id`),
   ADD KEY `files_created_by_foreign` (`created_by`),
   ADD KEY `files_deleted_by_foreign` (`deleted_by`);
+
+--
+-- Indexes for table `host_details`
+--
+ALTER TABLE `host_details`
+  ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -109,13 +130,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `file_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `file_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` smallint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -127,6 +148,12 @@ ALTER TABLE `users`
 ALTER TABLE `files`
   ADD CONSTRAINT `files_created_by_foreign` FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `files_deleted_by_foreign` FOREIGN KEY (`deleted_by`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `host_details`
+--
+ALTER TABLE `host_details`
+  ADD CONSTRAINT `host_details_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
