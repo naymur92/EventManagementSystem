7449757660b89fa4d31ce79c77c6fdeb616e5f27
Naymur Rahman <naymurrahman@yahoo.com>
2025-01-28 23:12:27 +0600

    added banner get method in Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,4 +11,17 @@ class Event extends BaseModel
     protected $table = 'events';
     protected $primaryKey = 'event_id';
     protected array $fillable = ['name', 'location', 'google_map_location', 'description', 'start_time', 'end_time', 'max_capacity', 'registration_fee', 'current_capacity', 'status', 'user_id', 'created_at', 'updated_at'];
+
+
+    /**
+     * Get banner image
+     *
+     * @return array|null
+     */
+    public function getBanner(): array|null
+    {
+        $files = $this->getFiles();
+
+        return array_filter($files, fn($file) => $file['fileinfo'] === 'banner_image')[0] ?? null;
+    }
 }
